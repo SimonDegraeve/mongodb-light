16b81821ca3d54c672c39788c9949ec415711d6b
Simon Degraeve <simon.degraeve@gmail.com>
2016-11-09 15:42:57 +0800

    noop: Add tests

---
+++ src/__tests__/utils.test.js
@@ -0,0 +1,61 @@
+/**
+ *
+ */
+import { toSort, toFields, toResult } from '../utils';
+
+/**
+ *
+ */
+describe('toSort()', () => {
+  it('let object through', () => {
+    expect(toSort({ a: 1 })).toEqual({ a: 1 });
+  });
+
+  it('returns an object from a string', () => {
+    expect(toSort('a -b')).toEqual({ a: 1, b: -1 });
+  });
+
+  it('supports custom string separator', () => {
+    expect(toSort('a,-b', ',')).toEqual({ a: 1, b: -1 });
+  });
+});
+
+describe('toFields()', () => {
+  it('returns the same object', () => {
+    expect(toFields({ a: 1 })).toEqual({ a: 1 });
+  });
+
+  it('returns an object from a string', () => {
+    expect(toFields('a -b')).toEqual({ a: 1, b: 0 });
+  });
+
+  it('supports custom string separator', () => {
+    expect(toFields('a,-b', ',')).toEqual({ a: 1, b: 0 });
+  });
+});
+
+describe('toResult', () => {
+  it('let object through', () => {
+    expect(toResult({ a: 1 })).toEqual({ a: 1 });
+  });
+
+  it('returns a transformed document', () => {
+    expect(toResult({ _id: 1 })).toEqual({ id: 1 });
+  });
+
+  it('returns a transformed document from value', () => {
+    expect(toResult({ value: { _id: 1 } })).toEqual({ id: 1 });
+  });
+
+  it('returns undefined from null value', () => {
+    expect(toResult({ value: null })).toEqual(undefined);
+  });
+
+  it('returns an array of transformed documents', () => {
+    expect(toResult([{ _id: 1 }, { _id: 2 }])).toEqual([{ id: 1 }, { id: 2 }]);
+  });
+
+  it('returns an array of transformed documents from ops', () => {
+    expect(toResult({ ops: [{ _id: 1 }, { _id: 2 }] })).toEqual([{ id: 1 }, { id: 2 }]);
+  });
+});
